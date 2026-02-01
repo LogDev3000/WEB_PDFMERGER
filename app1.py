ba6ceec34226de52a054d07a1e397686222dc7a6
LogDev3000 <jameslogan708@gmail.com>
2026-02-01 16:42:01 -0500

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import PyPDF2
+
+
+def merge_pdfs(output_path, pdf_documents):
+
+    pdf_merger = PyPDF2.PdfMerger()
+
+    for pdf_document in pdf_documents:
+
+        pdf_merger.append(pdf_document)
+
+    with open(output_path, 'wb') as output_file:
+        pdf_merger.write(output_file)
+
+
+def main():
+    st.image('assets/pdf.png')
+    st.header('UNIR PDFs')
+    st.subheader('Adjunta tus archivos PDF')
+
+
+    attached_pdfs = st.file_uploader(label = '',accept_multiple_files=True )
+
+    merge_button = st.button(label='MEZCLAR PDFs')
+
+    if merge_button:
+
+        if len(attached_pdfs ) <= 1:
+
+            st.warning('Adjunta más de un PDF')
+
+        else:
+            output_pdf = 'assets/pdf_final.pdf'
+            merge_pdfs(output_pdf,attached_pdfs)
+            st.success('Los archivos PDF se fusionaron correctamente')
+
+            with open(output_pdf, 'rb') as file:
+                pdf_data = file.read()
+            st.download_button(label = 'DESCARGAR PDF MEZCLADO', data=pdf_data, file_name='pdf_final.pdf')
+
+
+if __name__ == '__main__':
+    main()
